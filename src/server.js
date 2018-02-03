537bb5fbdd8b61733b4681fc04581cb21f95d530
Saurav Sahu <sauravsahu.12@live.com>
2018-02-03 11:55:38 +0530

    create hapi server
    create hapi server

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const hapi = require('hapi');
+
+const allRoutes = require('./routes');
+
+const server = new hapi.Server();
+const port = Number(process.env.PORT || process.argv[2] || 8080);
+
+server.connection({
+  host: 'localhost',
+  port,
+});
+
+server.route(allRoutes);
+
+if (!module.parent) {
+  server.start()
+    .then(() => {
+      console.log(`Server running at: ${server.info.uri}`);
+    })
+    .catch((error) => { throw error; });
+}
+
+module.exports = server;
